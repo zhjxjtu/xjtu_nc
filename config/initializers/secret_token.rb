2c149c6f8204e7d9fb9c91e9fa34cfd7b301bde1
Jing Zhang <zhjxjtu@gmail.com>
2012-09-06 15:55:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XjtuNc::Application.config.secret_token = '42b257043b3509270ecea56203b2a1401ad42c981b140b0b0e5f23ef6308c0f80d3a6d739a7a4ba33b0d2df908950354347341f32912ff67dbd065d54b67b077'
